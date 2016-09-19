52be77cbd7dc73a9544f3189ff12435a0850f5ec
Semjon Gluthkov <semen.glushkov.m@gmail.com>
2016-09-19 03:12:49 +0300

    old changes

--- lab3/lab-3-7.py
+++ lab3/lab-3-7.py
@@ -2,10 +2,17 @@ import turtle
 import math
 
 turtle.shape('turtle')
-m = 0.01
-n = 1000
-turtle.speed(100)
+dphi =  0.1
+phi = 0
+k = 2
+radius = 0
+n = 10000
+turtle.speed(10000)
+turtle.left(90)
 for i in range(n):
-	turtle.forward(m)
-	turtle.left(1)
-	m += 0.001
+	x = radius * math.cos(phi)
+	y = radius * math.sin(phi)
+	turtle.goto(x, y)
+	phi += dphi
+	turtle.left(180 / math.pi * dphi)
+	radius = k * phi
